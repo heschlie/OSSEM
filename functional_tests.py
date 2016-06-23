15965558da70ea5d62b1acf027f52de2e55db93b
Stephen Schlie <stephen_schlie@dell.com>
2016-06-23 09:38:32 -0700

    actually adding function_tests.py

---
+++ functional_tests.py
@@ -0,0 +1,22 @@
+import unittest
+from unittest import TestCase
+
+
+class TestReadMe(TestCase):
+    """
+    Cause, why not?  We want to make sure the README is always a valid format
+    to be rendered.
+    """
+
+    def test_readme_valid(self):
+        import rstcheck
+
+        with open('README.rst') as f:
+            rst = f.read()
+            errors = list(rstcheck.check(rst))
+
+            self.assertTrue(len(errors) == 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
